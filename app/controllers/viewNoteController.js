5f4264fd7205c12ecbe461551792fffcf07f9a5f
unknown <samuelrichard0309@gmail.com>
2024-11-03 23:59:20 +0700

    made change in viewNote to fix the edit, but still doesnt work

--- app/controllers/viewNoteController.js
+++ app/controllers/viewNoteController.js
@@ -1,6 +1,10 @@
 var noteApp = angular.module('noteApp')
 
-.controller('viewNoteController', function($scope, $routeParams) {
+.controller('viewNoteController', function($scope, $routeParams, $location) {
     const notes = JSON.parse(localStorage.getItem('notes'));
     $scope.note = notes[$routeParams.id];
+    
+    $scope.editNote = function(id) {
+        $location.path('/edit/' + id);
+    };
 });
